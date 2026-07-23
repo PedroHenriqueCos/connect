14cc3542068b72cb8b26d6fe88a642a2be5022d1
PedroHenriqueCos <phlkpedro@gmail.com>
2026-07-23 10:20:53 -0300

    Primeira versão do código

---
+++ src/test/kotlin/br/uerj/connect/ConnectApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.uerj.connect
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConnectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
